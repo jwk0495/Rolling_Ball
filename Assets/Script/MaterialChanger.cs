d444f334ad7fb4e889fbfaa91ecfccd95ae48bdc
jwk0495 <155418911+jwk0495@users.noreply.github.com>
2025-06-04 17:10:58 +0900

    SFX

--- Assets/Script/MaterialChanger.cs
+++ Assets/Script/MaterialChanger.cs
@@ -5,33 +5,40 @@ public class MaterialChanger : MonoBehaviour, IPointerEnterHandler, IPointerExit
 {
     public Material baseMaterial;
     public Material selectedMaterial;
+    public AudioClip clip;
+
     public GameObject particle;
 
     [SerializeField] private Renderer renderer;
+    private AudioSource source;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material = baseMaterial;
+
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(clip);
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         renderer.material = selectedMaterial;
+        source.PlayOneShot(clip);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         renderer.material = baseMaterial;
+        source.PlayOneShot(clip);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         particle.SetActive(!particle.activeSelf);
+        source.PlayOneShot(clip);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        renderer = GetComponent<Renderer>();
-        renderer.material = baseMaterial;
-    }
-
-    // Update is called once per frame
     void Update()
     {
     }
